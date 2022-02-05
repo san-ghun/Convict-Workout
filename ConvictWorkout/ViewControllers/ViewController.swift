b5fe2a254aac15979a9a1c5fa823a0d13206e0f5
Sanghun <po4865@gmail.com>
2022-02-05 15:42:48 +0900

    Update ViewController.swift - to play with YouTubeiOSPlayerHelper - to find proper method to provide video with playlist

--- ConvictWorkout/ViewControllers/ViewController.swift
+++ ConvictWorkout/ViewControllers/ViewController.swift
@@ -6,14 +6,34 @@
 //
 
 import UIKit
+import YouTubeiOSPlayerHelper
 
 class ViewController: UIViewController {
     
+    var testId = [
+        "channelId": "UCXQa6imK9KIZftTSLj4mxoQ",
+        "playlistId-1": "PLZsDuSZDsUoYrbiP5mz_ifYxN8h0VVgDL",
+        "playlistId-2": "PLZsDuSZDsUobxrB_p1RSM_bhESzbmzpDG",
+        "playlistId-3": "PLZsDuSZDsUobzRTk7yzeSFXQ8V8skuhnZ",
+        "playlistId-4": "PLZsDuSZDsUoYOuq2RgSqZSV-11BdHYViN",
+        "playlistId-5": "PLZsDuSZDsUoYei53KC1D7qKe4ehUdrIrP"
+    ]
+    
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var playerView: YTPlayerView!
+    @IBOutlet weak var textView: UITextView!
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        titleLabel.text = ""
+        dateLabel.text = ""
+        textView.text = ""
+        
+        self.playerView.load(withPlaylistId: testId["playlistId-1"]!, playerVars: ["index": 2, "playsinline": 1])
     }
 
 
