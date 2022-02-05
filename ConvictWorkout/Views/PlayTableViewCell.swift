43d7cdac0b78c468bc255def76610d6034cc01d6
Sanghun <po4865@gmail.com>
2022-02-05 14:05:39 +0900

    Add comments and connect UI components in TableViewCells

--- ConvictWorkout/Views/PlayTableViewCell.swift
+++ ConvictWorkout/Views/PlayTableViewCell.swift
@@ -6,8 +6,16 @@
 //
 
 import UIKit
+import YouTubeiOSPlayerHelper
 
 class PlayTableViewCell: UITableViewCell {
+    
+    // MARK: - Properties
+    
+    // MARK: IBOutlets
+    @IBOutlet weak var youtubePlayerView: YTPlayerView!
+    @IBOutlet weak var titleLabel: UILabel!
+    
 
     override func awakeFromNib() {
         super.awakeFromNib()
